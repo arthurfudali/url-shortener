d16637716c88a8b84961fccf4b10b9d6210bacc2
arthurfudali <arthur.fudali@gmail.com>
2026-02-05 12:00:58 -0300

    feat: add generateShortUrl and createShortUrl methods

--- src/main/java/dev/url_shortener/services/UrlService.java
+++ src/main/java/dev/url_shortener/services/UrlService.java
@@ -1,7 +1,11 @@
 package dev.url_shortener.services;
 
+import dev.url_shortener.entities.Url;
 import dev.url_shortener.repositories.UrlRepository;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UrlService {
@@ -12,4 +16,17 @@ public class UrlService {
         this.urlRepository = urlRepository1;
     }
 
+    public Url createShortUrl(String originalUrl) {
+        Url url = new Url();
+        url.setOriginalUrl(originalUrl);
+        url.setShortUrl(generateShortUrl(originalUrl));
+        url.setViews(0);
+        url.setExpiresAt(LocalDateTime.now().plusDays(1));
+        urlRepository.save(url);
+        return url;
+    }
+
+    private String generateShortUrl(String url) {
+        return RandomStringUtils.insecure().nextAlphabetic(5, 11);
+    }
 }
