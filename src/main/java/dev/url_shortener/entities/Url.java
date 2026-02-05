c87cf19fcbe62880514ba82a25c5d69d87eb36bf
arthurfudali <arthur.fudali@gmail.com>
2026-02-05 11:32:37 -0300

    feat: create entity URL

---
+++ src/main/java/dev/url_shortener/entities/Url.java
@@ -0,0 +1,95 @@
+package dev.url_shortener.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String originalUrl;
+    private String shortUrl;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+    private LocalDateTime expiresAt;
+    private Integer views;
+
+    public Url() {
+    }
+
+    public Url(Long id, String originalUrl, String shortUrl, LocalDateTime createdAt, LocalDateTime modifiedAt, LocalDateTime expiresAt, Integer views) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.expiresAt = expiresAt;
+        this.views = views;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+}
