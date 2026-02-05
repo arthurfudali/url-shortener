043ad5a5ef7e959f45c2bab26a671ec26397f2e9
arthurfudali <arthur.fudali@gmail.com>
2026-02-05 14:32:09 -0300

    feat: add h2 database suport and creates endpoint to shorten url

--- src/main/java/dev/url_shortener/controllers/UrlController.java
+++ src/main/java/dev/url_shortener/controllers/UrlController.java
@@ -1,5 +1,27 @@
 package dev.url_shortener.controllers;
 
+import dev.url_shortener.entities.Url;
+import dev.url_shortener.services.UrlService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UrlController {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/shorten-url")
+    public ResponseEntity<Url> shortenUrl(@RequestBody String originalUrl) {
+        Url shortUrl = urlService.createShortUrl(originalUrl);
+        return ResponseEntity.ok(shortUrl);
+    }
+
+
 
 }
