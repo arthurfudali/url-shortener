04515f84bfc73f6db6232f36509504ae5e8b9787
arthurfudali <arthur.fudali@gmail.com>
2026-02-05 14:39:00 -0300

    feat: creates redirect endpoint

--- src/main/java/dev/url_shortener/controllers/UrlController.java
+++ src/main/java/dev/url_shortener/controllers/UrlController.java
@@ -2,10 +2,12 @@ package dev.url_shortener.controllers;
 
 import dev.url_shortener.entities.Url;
 import dev.url_shortener.services.UrlService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 public class UrlController {
@@ -22,6 +24,17 @@ public class UrlController {
         return ResponseEntity.ok(shortUrl);
     }
 
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirectToUrl(@PathVariable String shortUrl) {
+        try {
+            String originalUrl = urlService.getOriginalUrl(shortUrl);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(URI.create(originalUrl));
+            return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 
 
 }
